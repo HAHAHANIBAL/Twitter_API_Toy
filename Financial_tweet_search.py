b7cd94204d32e0dd8f264478361fa915ff434c59
Mo Chen <kidrock2006@gmail.com>
2014-03-01 08:51:53 -0500

    Updates
    1. Added some parameters

--- Financial_tweet_search.py
+++ Financial_tweet_search.py
@@ -7,17 +7,21 @@ from twython import Twython, TwythonError
 
 #Requires Authentication as of Twitter API v1.1
 #Input custom API
-twitter = Twython('APIKey', 'APISecret', 'TokenKey', 'TokenSecret')
+twitter = Twython('APIKEY', 'APISECRET', 'TOKEN', 'TOKENSECRET')
 try:
 #GET search/tweets documentation
 #search options can be found here, date, count, language, etc.
-    search_results = twitter.search(q='#S&P500'.encode('utf-8'), count=100, lang='en')
+    search_results = twitter.search(q='love'.encode('utf-8'), count=50,lang='en',result_type='popular',geocode='37.781157,-122.398720,10mi')
 except TwythonError as e:
     print e
 
-
+count=0
 for tweet in search_results['statuses']:
+    count+=1
     #search single date tweet, looking for pre-defined parameters..
-    if "Feb 24" in tweet['created_at']:
+    print tweet['text'], '\n'
+    if "Feb 25" in tweet['created_at']:
         print 'Tweet from @%s Date: %s' % (tweet['user']['screen_name'], tweet['created_at'])
         print tweet['text'], '\n'
+
+print count
